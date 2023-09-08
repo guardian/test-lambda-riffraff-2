f7dfe73e2a963df12fc66ecc1e4f9aec864b05db
akash1810 <akash1810@users.noreply.github.com>
2023-09-08 11:13:30 +0100

    feat(lambda): Add infrastructure for lambda

--- packages/cdk/lib/example-typescript-lambda.ts
+++ packages/cdk/lib/example-typescript-lambda.ts
@@ -1,9 +1,18 @@
 import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
 import { GuStack } from '@guardian/cdk/lib/constructs/core';
+import { GuLambdaFunction } from '@guardian/cdk/lib/constructs/lambda';
 import type { App } from 'aws-cdk-lib';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class ExampleTypescriptLambda extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
+
+		new GuLambdaFunction(this, 'ExampleTypescriptLambda', {
+			app: 'example-typescript-lambda',
+			fileName: 'example-typescript-lambda.zip',
+			handler: 'index.main',
+			runtime: Runtime.NODEJS_18_X,
+		});
 	}
 }
