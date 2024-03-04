223e03ff7a83f42a6640b8c5cbb4761bde692dac
Julia Branke <Julia.Branke@guardian.co.uk>
2024-03-04 13:59:16 +0000

    Adjusted fro test-lambda-riffraff-2

--- packages/lambda/src/index.ts
+++ packages/lambda/src/index.ts
@@ -2,7 +2,7 @@ import { getConfig } from './config';
 
 export async function main() {
 	const { stage, app } = getConfig();
-	const msg = `Hello from ${app} in ${stage}, repo test-lambda-riffraff-1! The time is ${new Date().toString()}`;
+	const msg = `Hello from ${app} in ${stage}, repo test-lambda-riffraff-2! The time is ${new Date().toString()}`;
 	console.log(msg);
 	return Promise.resolve(msg);
 }
